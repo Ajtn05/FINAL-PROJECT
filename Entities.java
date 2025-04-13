a02456cdebaa41ccb3f6252a4944a033a1929130
jiji096 <janelle.angela.lopez@student.ateneo.edu>
2025-04-14 01:10:46 +0800

    new player assets

--- Entities.java
+++ Entities.java
@@ -2,8 +2,12 @@ import java.awt.image.*;
 
 public class Entities {
     public int x, y, speed;
-    public BufferedImage boyLeft1, boyRight1, boyUp1, boyDown1, boyLeft2, boyRight2, boyUp2, boyDown2;
-    public BufferedImage spriteImages;
+    public String character;
+    public BufferedImage boyLeft1, boyRight1, boyUp1, boyDown1, 
+                         boyLeft2, boyRight2, boyUp2, boyDown2, 
+                         girlUp1, girlDown1, girlLeft1, girlRight1, 
+                         girlUp2, girlDown2, girlLeft2, girlRight2;
+    public BufferedImage boyUp, boyDown, boyLeft, boyRight, girlUp, girlDown, girlLeft, girlRight;
     public String direction;
 
     public int spriteCounter = 0;
